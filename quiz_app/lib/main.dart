cdad9351b7b1d66ff33124f60c3155ec3ea88b16
Amoghtech <amogh.1923cs1192@kiet.edu>
2021-11-10 20:30:18 +0530

    Column, RaisedButton, Child, Onpressed

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -7,14 +7,46 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionindex=0;
+
+  void answerquestions() {
+    questionindex = questionindex + 1;
+    // print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext ctx) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App!'),
         ),
-        body: Text("This is my default text!"),
+        body: Column(children: [
+          Text(questions[questionindex]),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerquestions,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () => print("Answer 2 "),
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () {
+              print("Answer 3");
+            },
+          ),
+          RaisedButton(
+            child: Text('Answer 4'),
+            onPressed: answerquestions,
+          ),
+        ]),
       ),
     );
   }
