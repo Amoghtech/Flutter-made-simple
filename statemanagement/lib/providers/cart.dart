1e31781139a4f1f0ca18a96a513a12004b2c4b89
Amoghtech <amogh.1923cs1192@kiet.edu>
2022-01-14 19:18:20 +0530

    SnackBar,AlertDialog,ConfirmDissimisable

--- statemanagement/lib/providers/cart.dart
+++ statemanagement/lib/providers/cart.dart
@@ -67,7 +67,20 @@ class Cart with ChangeNotifier {
     _items.remove(productId);
     notifyListeners();
   }
+void removesingleitem(String productId){
+  if(!_items.containsKey(productId)){
+    return;
+  }
 
+
+  if(_items[productId].quantity>1){
+    _items.update(productId, (value) => CartItem(title:value.title,price:value.price,id: value.id,quantity: value.quantity-1));
+  }
+  else{
+    _items.remove(productId);
+  }
+  notifyListeners();
+}
   void clear(){
     _items={};
     notifyListeners();
